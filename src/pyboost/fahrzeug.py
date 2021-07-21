db44b1c2cd4048f229fa5096655ccf3ab2fe34ec
Christian Lück <christian.lueck@uni-muenster.de>
2021-07-21 08:01:45 +0200

    only ascii characters, vision sensor sample code

--- src/pyboost/fahrzeug.py
+++ src/pyboost/fahrzeug.py
@@ -50,7 +50,7 @@ class Einachser(MoveHub):
 
     def drehe(self, winkel, richtung = LINKS, geschwindigkeit = .5):
         """Drehe das Fahrzeug auf der Stelle um den Winkel winkel in der Richtung richtung herum."""
-        # Bei einer 360°-Drehung muss jedes Rad einen Bogen von 180°
+        # Bei einer 360-Drehung muss jedes Rad einen Bogen von 180
         # mit dem halben Radstand als Radius fahren.
         strecke = 0.5 * self.PI * self.spurweite
         geschwindigkeit_a = richtung * geschwindigkeit
@@ -59,34 +59,43 @@ class Einachser(MoveHub):
 
     def fahre_bogen(self, winkel, radius, richtung = LINKS, geschwindigkeit = .2):
         """Fahre das Fahrzeug im Kreisbogen mit dem Radius radius und der
-        Länge winkel. Der Radius ist der Innenradius."""
+        Laenge winkel. Der Radius ist der Innenradius."""
         if radius == 0:
             radius = 0.001 # Teilung durch 0 nicht definiert
         strecke_innen = 2 * self.PI * radius * winkel / 360
-        strecke_außen = 2 * self.PI * (radius + self.spurweite) * winkel / 360
-        strecke = (strecke_innen + strecke_außen) / 2
+        strecke_aussen = 2 * self.PI * (radius + self.spurweite) * winkel / 360
+        strecke = (strecke_innen + strecke_aussen) / 2
         geschwindigkeit_innen = geschwindigkeit
-        # die Geschwindigkeit außen muss größer sein:
-        geschwindigkeit_außen = (1 + (self.spurweite / radius)) * geschwindigkeit_innen
-        if geschwindigkeit_außen > 1:
-            geschwindigkeit_außen = 1
-            geschwindigkeit_innen = (radius / (self.spurweite + radius)) # * geschwindigkeit_außen
+        # die Geschwindigkeit aussen muss groesser sein:
+        geschwindigkeit_aussen = (1 + (self.spurweite / radius)) * geschwindigkeit_innen
+        if geschwindigkeit_aussen > 1:
+            geschwindigkeit_aussen = 1
+            geschwindigkeit_innen = (radius / (self.spurweite + radius)) # * geschwindigkeit_aussen
         if richtung == LINKS:
-            geschwindigkeit_a = geschwindigkeit_außen
+            geschwindigkeit_a = geschwindigkeit_aussen
             geschwindigkeit_b = geschwindigkeit_innen
         else:
             geschwindigkeit_a = geschwindigkeit_innen
-            geschwindigkeit_b = geschwindigkeit_außen
+            geschwindigkeit_b = geschwindigkeit_aussen
         print("Geschwindigkeiten in der Bogenfahrt, A: %.2f, B: %.2f" % (geschwindigkeit_a, geschwindigkeit_b))
         self.motor_AB.angled(self.winkel_zur_strecke(strecke), geschwindigkeit_a, geschwindigkeit_b)
 
 
 # Test
 if __name__ == "__main__":
+    from time import sleep
     fahrzeug = Einachser()
     fahrzeug.fahre_geradeaus(200)
     fahrzeug.drehe(180, RECHTS)
-    fahrzeug.fahre_bogen(90, 100)
+    fahrzeug.fahre_bogen(90, 500)
     fahrzeug.fahre_bogen(30, 50, RECHTS, geschwindigkeit = 1)
     fahrzeug.fahre_bogen(45, 0)
+    print("Entfernung: %f" % fahrzeug.distanz)
+    fahrzeug.motor_AB.start_power(.5)
+    sleep(1)
+    print("Entfernung: %f" % fahrzeug.distanz)
+    while fahrzeug.distanz > 5:
+        pass
+    print("Entfernung: %f" % fahrzeug.distanz)
+    fahrzeug.motor_AB.stop()
     fahrzeug.ende()
